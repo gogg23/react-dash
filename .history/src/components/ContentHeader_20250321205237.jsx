d101c48d33747f38ba661bafd0c55cb1bcddd67c
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-03-21 21:23:28 +0300

    commit

--- .history/src/components/ContentHeader_20250321205237.jsx
+++ .history/src/components/ContentHeader_20250321205237.jsx
@@ -6,7 +6,7 @@ const ContentHeader = () => {
     <div className='content--header'>
       <h1 className='header--title'>Dashboard</h1>
       <div className='header--activity'>
-        <div className='header-searchbox'>
+        <div className='search-box'>
           <input type='text' placeholder='Search anything here...' />
           <BiSearch className='icon' />
         </div>
